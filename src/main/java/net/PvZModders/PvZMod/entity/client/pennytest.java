1a24acb75743e7e089538a6f31a2b84ea1f38374
1MasterD1 <43497470+1MasterD1@users.noreply.github.com>
2026-04-30 01:08:14 -0400

    Fix Penny Blockbench model imports

--- src/main/java/net/PvZModders/PvZMod/entity/client/pennytest.java
+++ src/main/java/net/PvZModders/PvZMod/entity/client/pennytest.java
@@ -1,11 +1,25 @@
-// Made with Blockbench 5.1.4
+package net.PvZModders.PvZMod.entity.client;// Made with Blockbench 5.1.4
 // Exported for Minecraft version 1.17 or later with Mojang mappings
 // Paste this class into your mod and generate all required imports
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.PvZModders.PvZMod.PvZ2Mod;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 
 public class pennytest<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "pennytest"), "main");
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(PvZ2Mod.MOD_ID, "pennytest"), "main");
 	private final ModelPart body;
 	private final ModelPart portal;
 	private final ModelPart wheels;
