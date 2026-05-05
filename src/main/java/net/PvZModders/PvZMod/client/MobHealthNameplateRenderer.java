56f5fe21bcd7e6884559f31edd28ea8496425d47
1MasterD1 <43497470+1MasterD1@users.noreply.github.com>
2026-05-05 15:25:18 -0400

    Fix plant nameplates and wave gameplay polish

--- src/main/java/net/PvZModders/PvZMod/client/MobHealthNameplateRenderer.java
+++ src/main/java/net/PvZModders/PvZMod/client/MobHealthNameplateRenderer.java
@@ -1,6 +1,7 @@
 package net.PvZModders.PvZMod.client;
 
 import net.PvZModders.PvZMod.PvZ2Mod;
+import net.PvZModders.PvZMod.progression.seed.PlantEntityManager;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Mob;
 import net.minecraftforge.api.distmarker.Dist;
@@ -17,6 +18,9 @@ public final class MobHealthNameplateRenderer {
     @SubscribeEvent
     public static void renderMobHealth(RenderNameTagEvent event) {
         if (!(event.getEntity() instanceof Mob mob) || !mob.isAlive()) {
+            return;
+        }
+        if (PlantEntityManager.isPlant(mob)) {
             return;
         }
 
